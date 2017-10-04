13e458de9d963cca8ba3f5fb6b458867097cb4ea
Corey Rodems <coreyrodems@gmail.com>
2017-10-03 21:41:16 -0700

    added html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,13 @@
 var path = require("path");
 
 module.exports = function(app){
-    app.get("/", function(req, res) {
+    app.get("/game", function(req, res) {
         res.sendFile(path.join(__dirname+'/game.html'))
-      });
+    });
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname+'/login.html'))
+    });
+    app.get("/scores", function(req, res) {
+        res.sendFile(path.join(__dirname+'/game.html'))
+    });
 }
