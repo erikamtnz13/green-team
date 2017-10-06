ad9d00442bc42d032ac9d2fc8cdac162d96f2762
Corey Rodems <coreyrodems@gmail.com>
2017-10-05 21:04:34 -0700

    !

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,9 +5,11 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname+'/../public/html/game.html'))
     });
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname+'/login.html'))
+        // res.sendFile(path.join(__dirname+'/login.html'))
+        res.end()
     });
     app.get("/scores", function(req, res) {
-        res.sendFile(path.join(__dirname+'/game.html'))
+        // res.sendFile(path.join(__dirname+'/game.html'))
+        res.end()
     });
 }
