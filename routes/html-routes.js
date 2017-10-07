69f581ce52d19b1e1f3f0d31b6bfb6da6fff7ab0
Corey Rodems <coreyrodems@gmail.com>
2017-10-07 13:09:27 -0700

    Merged w/ master

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,12 +4,5 @@ module.exports = function(app){
     app.get("/game", function(req, res) {
         res.sendFile(path.join(__dirname+'/../public/html/game.html'))
     });
-    app.get("/", function(req, res) {
-        // res.sendFile(path.join(__dirname+'/login.html'))
-        res.end()
-    });
-    app.get("/scores", function(req, res) {
-        // res.sendFile(path.join(__dirname+'/game.html'))
-        res.end()
-    });
+
 }
--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+	defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 require("./routes/html-routes.js")(app);
