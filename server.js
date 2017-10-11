0c05f084dab4132a69b7fa02cdaaeb75703ed893
PeterSurowski <petersurowski@yahoo.com>
2017-10-10 20:34:54 -0700

    Third commit

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
+// Setting up Passport:
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 
 // Import routes and give the server access to them.
 require("./routes/html-routes.js")(app);
